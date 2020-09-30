a18e272006538c740754ae3748dc5f56840c4541
Alexander Pfyffer <alexander.pfyffer@gmail.com>
2020-09-30 21:04:32 +0200

    sending notification user that sent message (but doesn't really make sense)

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,44 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const { firestore } = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 exports.sendNotificationToTopic = functions.firestore.document('CHATS/{chatId}/MESSAGES/{messageId}').onWrite(async (event) => {
     //let docID = event.after.id;
     // let title = event.after.get('title');
-    // let content = event.after.get('content');
-    var registrationToken = 'fijJdl0IF0vgjx9_fJzQTA:APA91bGUnw_m3ru70BZ8-nmOddRyimbbdrahEB4y54I5DbUfn-aMTKbVXtr1fJ8uGhG1CQif8wHfV2PVBm-APZEbjS9zPd3BXhPw8M7DM_QMSCU2t3XWgIJ7SkRxDhsOV8G2cJuhu6eu';
+
+    console.log('event.after', event.after)
+
+    // get uid from MESSAGES
+    let uid = event.after.get('user')._id;
+    console.log('uid', uid)
+
+
+    //get userRef with fcmToken
+    var fcmToken = firestore.document('USERS/'+uid).get('fcmToken')
+    console.log('userRef', userRef)
+    
+
+    // var user = admin.auth().getUser('0wP2yO2hhqTWFXlDDeV3WLUS5dg1')
+    // .then((userRecord) => {
+    //   // See the UserRecord reference doc for the contents of userRecord.
+    //   console.log('Successfully fetched user data:', userRecord.toJSON());
+    //   return userRecord.toJSON()
+    // })
+    // .catch((error) => {
+    //   console.log('Error fetching user data:', error);
+    // });
+
+
     var message = {
         notification: {
             title: 'title',
             body: 'content',
         },
-        token: registrationToken,
+        token: fcmToken,
     };
+
+
 
     let response = await admin.messaging().send(message);
     console.log(response);
