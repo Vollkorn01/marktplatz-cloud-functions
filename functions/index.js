7591fdb7581dd379e37045a9b90abf5bd1f4ee82
Alexander Pfyffer <alexander.pfyffer@gmail.com>
2020-10-03 13:48:03 +0200

    notification so receiving user working :D

--- functions/index.js
+++ functions/index.js
@@ -9,48 +9,79 @@ exports.sendNotificationToTopic = functions.firestore.document('CHATS/{chatId}/M
 
     console.log('event.after', event.after)
 
-    // get uid from MESSAGES
-    let uid = event.after.get('user')._id;
-    console.log('uid', uid)
+    // get message text
+    let messageText = event.after.get('text');
 
-    let fcmToken;
-    const users = firestore().collection('USERS').doc(uid);
-    const doc = await users.get();
-    if (!doc.exists) {
-      console.log('No such document!');
+    // get uid from user that wrote the message
+    let uidSender = event.after.get('user')._id;
+
+    // get uid and name from user that requested the item
+    let requester = event.after.get('requester');
+    let uidRequester = requester.uid;
+    let nameRequester = requester.displayName;
+
+    // get uid and name from user that created the item
+    let createdBy = event.after.get('createdBy');
+    let uidCreatedBy = createdBy.uid;
+    let nameCreatedBy = createdBy.displayName;
+
+
+    // who is the receiver?
+    let uidReceiver;
+    let nameReceiver;
+    if (uidSender === uidRequester) {
+      // send message to uidCreatedBy
+      uidReceiver = uidCreatedBy
+      nameReceiver = nameCreatedBy
+    } else if (uidSender === uidCreatedBy) {
+      // send message to uidRequester
+      uidReceiver = uidRequester
+      nameReceiver = nameRequester
     } else {
-      console.log('Document data:', doc.data());
-      fcmToken = doc.data().fcmToken
+      console.log('Error: uidSender doesnt match, should never be the case')
     }
 
-    if (fcmToken) {
-    
 
-    // var user = admin.auth().getUser('0wP2yO2hhqTWFXlDDeV3WLUS5dg1')
-    // .then((userRecord) => {
-    //   // See the UserRecord reference doc for the contents of userRecord.
-    //   console.log('Successfully fetched user data:', userRecord.toJSON());
-    //   return userRecord.toJSON()
-    // })
-    // .catch((error) => {
-    //   console.log('Error fetching user data:', error);
-    // });
+    if (uidReceiver) {
+      let fcmToken;
+      const userDoc = firestore().collection('USERS').doc(uidReceiver);
+      const doc = await userDoc.get();
+      if (!doc.exists) {
+        console.log('No such document!');
+      } else {
+        console.log('Document data:', doc.data());
+        fcmToken = doc.data().fcmToken
+      }
+
+      if (fcmToken) {
+      
+
+      // var user = admin.auth().getUser('0wP2yO2hhqTWFXlDDeV3WLUS5dg1')
+      // .then((userRecord) => {
+      //   // See the UserRecord reference doc for the contents of userRecord.
+      //   console.log('Successfully fetched user data:', userRecord.toJSON());
+      //   return userRecord.toJSON()
+      // })
+      // .catch((error) => {
+      //   console.log('Error fetching user data:', error);
+      // });
 
 
-    var message = {
-        notification: {
-            title: 'title',
-            body: 'content',
-        },
-        token: fcmToken,
-    };
+      var message = {
+          notification: {
+              title: nameReceiver,
+              body: messageText,
+          },
+          token: fcmToken,
+      };
 
 
 
-    let response = await admin.messaging().send(message);
-    console.log(response);
-}
-console.log('fcmToken undefined, no notification sent to user', uid)
+      let response = await admin.messaging().send(message);
+      console.log(response);
+      console.log('uidReceiver', uidReceiver)
+    }
+  }
 });
 
 // exports.sendNotificationToFCMToken = functions.firestore.document('messages/{mUid}').onWrite(async (event) => {
